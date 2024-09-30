88bd3fe3494584d55bb99f00bdde3d773c70fd5c
rafael <162385496+rrafaleao@users.noreply.github.com>
2024-09-30 23:56:14 +0000

    rotas

--- app/app.py
+++ app/app.py
@@ -8,25 +8,5 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://app_user:QWxVbk9zVERz@alunostds
 
 db = SQLAlchemy(app)
 
-@app.route('/test_db')
-def test_db():
-    try:
-        db.session.execute(text("SELECT 1"))
-        return 'Conexão com o banco de dados estabelecida com sucesso!'
-    except Exception as e:
-        return str(e)
-
-@app.route('/')
-def main_menu():
-    return "Sistema de gerenciamento de salas iniciado"
-
-@app.route('/sobre')
-def sobre():
-    return "Este é o sistema para gerenciar salas e recursos da escola"
-
-@app.route('/contato')
-def contato():
-    return "Contato: admin@escola.com"
-
 if __name__ == '__main__':
     app.run(debug=True)
--- app/routes.py
+++ app/routes.py
@@ -21,3 +21,14 @@ Rafael:
 Register build
 Schedule classrooms
 """
+
+from flask import Flask, render_template
+from app import app
+
+@app.route('/schedule/classrooms', methods=[('POST')])
+def schedule_classroom():
+    return render_template('codigo.html')
+
+@app.route('/register-build', methods=[('POST')])
+def register_build():
+    return render_template('codigo.html')
