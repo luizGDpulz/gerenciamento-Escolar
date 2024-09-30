9bce3d5dd037c6364fd909627fc2acba606e2baa
Luiz Gustavo Dias Pulz <162385594+luizGDpulz@users.noreply.github.com>
2024-09-30 22:58:55 +0000

    tarefas rotas

---
+++ app/routes.py
@@ -0,0 +1,23 @@
+"""
+Luiz:
+
+Login
+Register student
+Schedule
+
+Kelvin:
+
+Dashboard
+Register class
+Schedule classrooms
+
+Matiele:
+
+MainFrame
+Register classrooms
+
+Rafael:
+
+Register build
+Schedule classrooms
+"""
