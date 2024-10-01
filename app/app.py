365f2d2374424a805ecc89bf5d1cd21cbae0cbbf
matielli <162385460+matielli@users.noreply.github.com>
2024-10-01 00:04:33 +0000

    a

--- app/app.py
+++ app/app.py
@@ -1,32 +1,15 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://app_user:QWxVbk9zVERz@alunostds.dev.br:3308/app_user'
+##app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://app_user:QWxVbk9zVERz@alunostds.dev.br:3308/app_user'
 
-db = SQLAlchemy(app)
+##db = SQLAlchemy(app)
 
-@app.route('/test_db')
-def test_db():
-    try:
-        db.session.execute(text("SELECT 1"))
-        return 'Conexão com o banco de dados estabelecida com sucesso!'
-    except Exception as e:
-        return str(e)
+salas = []
 
-@app.route('/')
-def main_menu():
-    return "Sistema de gerenciamento de salas iniciado"
-
-@app.route('/sobre')
-def sobre():
-    return "Este é o sistema para gerenciar salas e recursos da escola"
-
-@app.route('/contato')
-def contato():
-    return "Contato: admin@escola.com"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/register', methods=['POST'])
+def register_classroom():
+   return render_template('codigo.html')
--- app/routes.py
+++ app/routes.py
@@ -11,13 +11,32 @@ Dashboard
 Register class
 Schedule classrooms
 
-Matiele:
+Matielii:
 
 MainFrame
 Register classrooms
+
+
 
 Rafael:
 
 Register build
 Schedule classrooms
 """
+
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/register-classroom', methods=['POST'])
+def register_classroom():
+    return render_template('codigo.html')
+
+
+
+@app.route('/mainframe', methods=['POST'])
+def mainframe():
+    return render_template('codigo.html')
+
+   
