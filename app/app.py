093e904468a79a2e41c6c6d989f3fba83081ac8d
Luiz Gustavo Dias Pulz <162385594+luizGDpulz@users.noreply.github.com>
2024-10-02 00:12:04 +0000

    resolvido instancia sql alchemy

--- app/app.py
+++ app/app.py
@@ -1,9 +1,7 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from models import db
 from sqlalchemy.exc import SQLAlchemyError
 import time
-
-db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
